7d18a020a8c8e4ddfe67d0130ac51bbe407b7908
theCosmas <61983553+theCosmas@users.noreply.github.com>
2020-03-25 12:06:24 +0100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,18 @@
+#A Python program which accepts the radius of a circle
+#from the user and compute the area.
+
+
+import math
+from math import pi
+
+radius = input("Enter the radius of circle: ")
+
+print("Radius is: ", radius)
+
+Area_circle = pi*float(radius)**2
+
+print("Area of the circle is ", Area_circle)
+
+
+
+print("Slack username - @theCosmas")
